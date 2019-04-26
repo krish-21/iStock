ed67046c48445b4184696dd9d793582d0f4aab09
harshag14 <harshag1410@gmail.com>
2019-04-27 00:20:54 +0800

    Update start.cpp

--- start.cpp
+++ start.cpp
@@ -3,22 +3,38 @@
 #include <string>
 #include <fstream>
 
-#include "start.h"
-
 using namespace std;
+string newuser(Investor);
 
 struct users
 {
 	string name;
 	string userid;
 	string password;
+	double creditlimit;
+	double wallet_balance;
+	double networth;
+	double google;
+	double apple;
+	double microsoft;
+	double facebook;
+	double amazon;
 };
 
-bool newuser(int n)
+string newuser(Investor &user)
 {
-	
-	label1:
-	
+	cout<<"Welcome to iStock"<<endl;
+	cout<<"1- Create New Account"<<endl;
+	cout<<"2- Login"<<endl;
+	cout<<"3- Exit"<<endl;
+	int n;
+	cin>>n;
+	while(n<1 || n>3)
+	{
+		cout<<"Invalid Input"<<endl;
+		cout<<"Enter Value again"<<endl;
+		cin>>n;
+	}
 	if(n==1)
 	{
 		users newuser;
@@ -28,44 +44,67 @@ bool newuser(int n)
 		cin>>newuser.userid;
 		cout<<"Enter Password"<<endl;
 		cin>>newuser.password;
+		cout<<"Enter Your Credit Limit"<<endl;
+		cin>>newuser.creditlimit;
+		newuser.wallet_balance=0;
+		newuser.networth=0;
+		newuser.google=0;
+		newuser.apple=0;
+		newuser.microsoft=0;
+		newuser.facebook=0;
+		newuser.amazon=0;
 		ofstream foutusers;
 		foutusers.open("users.txt",ios::app);
 		if(foutusers.fail())
 		{
-			cout<<"Unable to open text file"<<endl;
+			cout<<"Unable to open users.txt"<<endl;
 			exit(1);
 		}
-		foutusers<<newuser.name<<" "<<newuser.userid<<" "<<newuser.password<<endl;
+		foutusers<<newuser.name<<" "<<newuser.userid<<" "<<newuser.password<<" "<<newuser.creditlimit<<" "<<newuser.wallet_balance<<" "<<newuser.networth
+		<<" "<<newuser.google<<" "<<newuser.apple<<" "<<newuser.microsoft<<" "<<newuser.facebook<<" "<<newuser.amazon;
 		foutusers.close();
-		return true;
+		cout<<"Successfully created your account"<<endl;
+		set_name(newuser.name);
+		set_net_worth();
+    	set_wallet_balance(newuser.wallet_balance);
+    	set_net_worth(newuser.networth);
+   		set_credit_limit(newuser.creditlimit);
+		
+		return newuser.userid;
 	}
-	
 	else if(n==2)
 	{
 		string uid,password;
 		cout<<"Enter User Id"<<endl;
 		cin>>uid;
 		ifstream finusers;
-		finusers.open("users.txt",ios::app);
+		finusers.open("users.txt");
 		if(finusers.fail())
 		{
-			cout<<"Unable to find you"<<endl;
+			cout<<"Unable to open users.txt"<<endl;
 			exit(1);
 		}
-		int n=10;
-		users a[10];
 		string line;
-		int initialize=0;
-		while(finusers>>a[initialize].name>>a[initialize].userid>>a[initialize].password)
+		int length;
+		while(getline(finusers,line))
 		{
-			initialize++;
+			length++;
+		}
+		users * userdetails=new users[length];
+		finusers.clear();
+		finusers.seekg(0, ios::beg);
+		int k=0;
+		while(finusers>>userdetails[k].name>>userdetails[k].userid>>userdetails[k].password>>userdetails[k].creditlimit>>userdetails[k].wallet_balance>>
+		newuser[k].networth>>userdetails[k].google>>userdetails[k].apple>>userdetails[k].microsoft>>userdetails[k].facebook>>userdetails[k].amazon)
+		{
+			k++;
 		}
 		int i,pos=-1;
 		while(pos<0)
 		{
-			for(i=0;i<n;i++)
+			for(i=0;i<length;i++)
 			{
-				if(a[i].userid==uid)
+				if(userdetails[i].userid==uid)
 				{
 					cout<<"Uid found"<<endl;
 					pos=i;
@@ -80,36 +119,24 @@ bool newuser(int n)
 			}	
 		}
 		cout<<"Enter Password"<<endl;
-		while(true)
+		cin>>password;
+		while(userdetails[pos]!=password)
 		{
+			cout<<"Incorrect Password"<<endl;
+			cout<<"Enter Password Again"<<endl;
 			cin>>password;
-			if(a[i].password==password)
-			{
-				cout<<"Login Successful"<<endl;
-				return true;
-			}
-			else
-			{
-				cout<<"Incorrect Password"<<endl;
-				cout<<"Enter Password Again"<<endl;
-			}
 		}
+		cout<<"Login Successful"<<endl;
+		set_name(userdetails[pos].name);
+    	set_net_worth(userdetails[pos].networth);
+    	set_wallet_balance(userdetails[pos].wallet_balance);
+    	set_credit_limit(userdetails[pos].creditlimit);
 	}
-
-	else
+	else if(n==3)
 	{
-		cout<<"Invalid input"<<endl<<endl;
-		goto label1;
+		cout<<"Thanks for using iStock"<<endl;
+		exit(1);
 	}
-	
 }
 
-void display()
-{
-	cout<<"Welcome to abcd"<<endl;
-	cout<<"1- Create New Account"<<endl;
-	cout<<"2- Login"<<endl;
-	int n;
-	cin>>n;
-	newuser(n);
-}
+
